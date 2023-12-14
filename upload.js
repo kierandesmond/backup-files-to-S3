71d5361aa818d39e8fb1c54a0c9900e1b9b66740
Kieran Desmond <34133202+kierandesmond@users.noreply.github.com>
2023-12-14 10:22:55 +0100

    Update upload.js
    Added a function to check if the file exists already in the bucket, if it does lets skip it, as we want to maintain the original metadata from exactly when it was uploaded.

--- upload.js
+++ upload.js
@@ -11,23 +11,55 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-function uploadFileToS3(filePath, bucketName) {
-    const fileContent = fs.readFileSync(filePath);
-    const fileName = path.basename(filePath);
-
+function checkFileExists(bucketName, fileName) {
     const params = {
         Bucket: bucketName,
-        Key: fileName,
-        Body: fileContent
+        Key: fileName
     };
 
-    s3.upload(params, function(err, data) {
-        if (err) {
-            console.error("Error uploading file:", err);
+    return new Promise((resolve, reject) => {
+        s3.headObject(params, function(err) {
+            if (err && err.code === 'NotFound') {
+                // File does not exist
+                resolve(false);
+            } else if (err) {
+                // An error occurred
+                reject(err);
+            } else {
+                // File exists
+                resolve(true);
+            }
+        });
+    });
+}
+
+async function uploadFileToS3(filePath, bucketName) {
+    const fileName = path.basename(filePath);
+
+    try {
+        const exists = await checkFileExists(bucketName, fileName);
+        if (exists) {
+            console.log(`File ${fileName} already exists in bucket. Skipping upload.`);
             return;
         }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
+
+        const fileContent = fs.readFileSync(filePath);
+        const params = {
+            Bucket: bucketName,
+            Key: fileName,
+            Body: fileContent
+        };
+
+        s3.upload(params, function(err, data) {
+            if (err) {
+                console.error("Error uploading file:", err);
+                return;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+    } catch (err) {
+        console.error("Error in upload process:", err);
+    }
 }
 
 function uploadPDFs(directoryPath, bucketName) {
